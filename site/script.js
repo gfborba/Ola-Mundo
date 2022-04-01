70bd58bbf5699caab7be392b7a1cd4b99a55ab4c
Guilherme Borba <guifborba@hotmail.com>
2022-04-01 18:39:56 -0300

    Modificações no site

---
+++ site/script.js
@@ -0,0 +1,11 @@
+let text = document.getElementById ('txt')
+
+function b1() {
+    text.innerHTML = '<p><strong>Lorem Ipsum</strong> é um texto muito conhecido entre profissionais do design e do TI. Ele se trata de um poema em latim, escrito por Cícero em 45 a.C, com as letras embaralhadas. Normalmente o poema é utilizado por desenvolvedores para preencher espaços para textos em branco, com a finalidade de criar noção de espaçamento, estilo, e etc.</p>'
+}
+function b2() {
+    text.innerHTML = '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab accusamus voluptas, rem quod accusantium numquam dolores distinctio rerum natus placeat id iusto aliquid ducimus delectus laborum voluptatibus adipisci architecto? Debitis!</p>'
+}
+function b3() {
+    text.innerHTML = '<p>"A dor em si é importante para a principal elite adiposa. Ser repudiado pelo inventor e a existência dessas dores deve ser evitado. Ele o condena como se eles não o conhecessem, e como aquele a quem eles seguem; bem-vindo a grande coisa que é abençoada"</p>'
+}
